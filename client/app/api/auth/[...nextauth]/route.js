66ac9b63b300756458e17fc295d4197447cf2fc3
Lauren Slayman <laurenaslayman@gmail.com>
2023-07-18 15:08:49 -0600

    oof

--- client/app/api/auth/[...nextauth]/route.js
+++ client/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,22 @@
-import NextAuth from 'next-auth'
-import { options } from './options'
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-const handler = NextAuth(options)
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Email',
+            credentials: {
+                email: {label: 'Email', type: 'text'},
+                password: {label: 'Password', type: 'password'}
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('Please enter an email and password')
+                }
+            }
+        })
+    ]
+}
 
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
